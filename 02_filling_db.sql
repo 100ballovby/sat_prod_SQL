6976ffc59ea101cd5106b748d878775d1e618fcc
demidraksin <greatraksin@icloud.com>
2022-02-05 17:59:48 +0300

    Inserting Values to DB

---
+++ 02_filling_db.sql
@@ -0,0 +1,10 @@
+INSERT INTO authors (name, birthdate)
+VALUES ('Eric Matthes', datetime('now')),
+        ('Paul Barry', datetime('1995-09-12')),
+        ('Mark Lutz', datetime('1968-07-02'));
+
+INSERT INTO publishers (name, country)
+VALUES ('O`Relly', 'New York'),
+       ('', ''),
+       ('', ''));
+
