3a435d9b962c145b94ac529bbab75df516cb4d60
demidraksin <greatraksin@icloud.com>
2022-02-05 18:15:31 +0300

    Inserting Values to DB

--- 02_filling_db.sql
+++ 02_filling_db.sql
@@ -7,6 +7,10 @@ INSERT INTO publishers (name, country)
 VALUES ('O`Relly', 'New York'),
        ('No Starch Press', 'Washington DC');
 
-
-
-
+INSERT INTO books (name, author_id, pub_date, publisher_id)
+VALUES ('Python Crash Course: A Hands-On, Project-Based Introduction to Programming (2nd Edition)',
+        4, date('2019-05-03'), 2),
+       ('Head-First Python: A Brain-Friendly Guide (2nd Edition)',
+        5, date('2016-12-16'), 1),
+       ('Programming Python: Powerful Object-Oriented Programming (4th Edition)',
+        6, date('2011-01-14'), 1);
