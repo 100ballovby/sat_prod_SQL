dd0c3997d24520bd59a2503c34bc655cfac059e9
demidraksin <greatraksin@icloud.com>
2022-02-05 17:48:47 +0300

    Creating new tables from file

--- 01_creating_tables.sql
+++ 01_creating_tables.sql
@@ -2,8 +2,25 @@ CREATE TABLE IF NOT EXISTS books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(255) NOT NULL,
     author_id INTEGER,
-    pub_date DATETIME,
-    publisher_id INTEGER
+    pub_date TEXT,
+    publisher_id INTEGER,
+
+    FOREIGN KEY (author_id)
+        REFERENCES authors (id),
+    FOREIGN KEY (publisher_id)
+        REFERENCES publishers (id)
+);
+
+CREATE TABLE IF NOT EXISTS authors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    birthdate TEXT
+);
+
+CREATE TABLE IF NOT EXISTS publishers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    country VARCHAR(30) NOT NULL
 );
 
 
